0eaf9f316b1e3768f29a551ae654a5c23fd6bbc1
Michael Reder <michael.reder@hofer.at>
2021-11-08 17:13:50 +0100

    File Manager can read XML Files

--- code/services/file_manager.py
+++ code/services/file_manager.py
@@ -1,15 +1,28 @@
 from bs4 import BeautifulSoup
 
 class FileManager:
-    _filePath = "../../dataset/wikipedia articles"
+    _filePath = "../../dataset/wikipedia articles/"
     _currentFileIndex = 0
 
     def __init__(self):
-        print("FileManager is created")
+        self._currentFileIndex = 0
 
     def readNextFile(self):
         self._currentFileIndex += 1
         return self.readFile(self._currentFileIndex)
 
-    def readFile(self, index):
-        return ""
+    def readFile(self, index: int):
+        contentArray: [str] = []
+
+        with open(self._filePath + str(index) + ".xml", 'r') as f:
+            dataStream = f.read()
+
+        data = BeautifulSoup(dataStream, 'lxml')
+        bodyElements = data.findAll("bdy")
+
+        # TODO [MiRe] Improve Code
+        for ele in bodyElements:
+            for content in ele.contents:
+                contentArray.append(content)
+
+        return contentArray
