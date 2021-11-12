14e96ff651b11de0bc347c53448af5b31491905a
Arianna Pera <54574943+ariannap13@users.noreply.github.com>
2021-11-12 14:53:39 +0100

    Added stemming, stop words and special characters

--- iir_code/createindex.py
+++ iir_code/createindex.py
@@ -4,9 +4,35 @@ This file contains your iir_code to create the inverted index. Besides implement
 from iir_code.inverted_index import InvertedIndex
 from iir_code.services.file_manager import FileManager
 import re
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import string
+import unicodedata
+
 
 fileManager = FileManager()
 invertedIndex = InvertedIndex()
+
+# stop words set
+sw_set = set(stopwords.words('english'))
+sw_set.update({''}) # add as stop words to remove empty strings
+
+# punctuation
+regular_punct = string.punctuation
+extra_punct = [',', '.', '"', ':', ')', '(', '!', '?', '|', ';', "'", '$', '&',
+    '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£',
+    '·', '_', '—', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',
+    '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', '“', '★', '”',
+    '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾',
+    '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', '▒', '：', '¼', '⊕', '▼',
+    '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲',
+    'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', '∙', '）', '↓', '、', '│', '（', '»',
+    '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø',
+    '¹', '≤', '‡', '√', '«', '»', '•', '´', 'º', '¾', '¡', '§', '£', '₤', 
+    '€', '·']
+extra_punct = "".join(extra_punct)
+punctuation = regular_punct + extra_punct
+
 
 
 # cachedFiles: {int: [str]} = {}
@@ -27,18 +53,39 @@ def processFile(content: [str], docID: int):
         token = text2tokens(text)
         invertedIndex.append(token, docID)
 
-
-# TODO: Do stemming, stopword removal
+# TODO: Should we remove accents (eg.: é->e)?
 def text2tokens(text: str):
-    text = text.lower()
-    splitedText = re.split(" |\n", text)
-    print(splitedText)
-
     """
     :param text: a text string
     :return: a tokenized string with preprocessing (e.g. stemming, stopword removal, ...) applied
     """
-    return ""
+    
+    # lowercase
+    text = text.lower()
+    
+    # remove all http addresses and www websites
+    text = re.sub("http[^\\s]+", "", text)
+    text = re.sub("www[^\\s]+", "", text)
+    
+    # remove all \xa chars
+    text = unicodedata.normalize("NFKD", text)
+    
+    #handle multi-blanks
+    text = re.sub(' +', ' ', text)
+           
+    # remove punctuation
+    text = text.translate(str.maketrans('', '', punctuation))
+        
+    # tokenization
+    splitedText = re.split(" |\n", text)
 
+    # stemming 
+    PS = PorterStemmer()
+    splitedText = [PS.stem(token) for token in splitedText]
+
+    # stop words removal
+    splitedText = [token for token in splitedText if token not in sw_set]
+
+    return splitedText
 
 main()
