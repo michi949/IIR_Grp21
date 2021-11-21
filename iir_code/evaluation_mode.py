824ed5ab039790ff65d10cafe4e336e313b2602d
Michael Reder <michael.reder@hofer.at>
2021-11-21 11:46:25 +0100

    added input function

--- iir_code/evaluation_mode.py
+++ iir_code/evaluation_mode.py
@@ -1,5 +1,5 @@
 """
-This file contains your iir_code to generate the evaluation files that are input to the trec_eval algorithm.
+This file contains your code to generate the evaluation files that are input to the trec_eval algorithm.
 """
 
 
--- iir_code/exploration_mode.py
+++ iir_code/exploration_mode.py
@@ -1,4 +1,21 @@
 """
-This file contains your iir_code for the interactive exploration mode where a string can be input by a user and a ranked list of documents is returned.
+This file contains your code for the interactive exploration mode where a string can be input by a user and a ranked list of documents is returned.
 Make sure that the user can switch between TF-IDF and BM25 scoring functions.
 """
+
+
+def main():
+    function_type = input('Choose your preferred scoring function (Enter TF-IDF or BM25):')
+    if function_type == "TF-IDF":
+        tf_idf()
+    elif function_type == "BM25":
+       bm25()
+    else:
+        print("No valid scoring function is selected")
+
+def tf_idf():
+
+def bm25():
+
+
+main()
