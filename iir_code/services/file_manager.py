3082df163b843e12edbd7b8dee593ec2e23a4745
Arianna Pera <54574943+ariannap13@users.noreply.github.com>
2021-11-22 18:25:50 +0100

    added with new ranking docs dict

--- iir_code/services/file_manager.py
+++ iir_code/services/file_manager.py
@@ -41,8 +41,10 @@ class FileManager:
         # Construct a list of tuples [(id, body)] with each article id and corresponding body
         id_body_list = tuple(zip(ids_cleaned, bodies_cleaned))
 
-        return id_body_list
+        # TODO [MiRe] Add description headers
 
+        return id_body_list
+    
     def read_topics_file(self):
         with open(self._topics_file_path, 'r') as f:
             data_stream = f.read()
@@ -60,13 +62,39 @@ class FileManager:
 
         return topics
 
-    def save_index_as_pickle(self, inverted_index: InvertedIndex):
-        print('Dumping index to disk...')
-        with open(self._saved_index_path, 'wb') as handle:
-            pickle.dump(inverted_index.dictionary, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    def save_index_to_pickle(self, inverted_index: InvertedIndex):
+        """
+        Store an index as a pickle file in inverted_index.pickle.
+        :param inverted_index: The inverted index to save to disk
+        """
+        with open(self._saved_index_path + 'inverted_index.pickle', 'wb') as handle:
+            pickle.dump([inverted_index.dictionary, inverted_index.ranking_dict], handle,
+                        protocol=pickle.HIGHEST_PROTOCOL)
 
-    def load_index_from_pickle(self):
-        print('Loading index from disk')
+    def load_index_from_pickle(self) -> InvertedIndex:
+        """
+        Load an index from a pickle file.
+        return: The index loaded from disk
+        """
+        with open(self._saved_index_path + 'inverted_index.pickle', 'rb') as handle:
+            index = pickle.load(handle)
+            inverted_index = InvertedIndex()
+            inverted_index.dictionary = index[0]
+            inverted_index.ranking_dict = index[1]
 
-        with open(self._saved_index_path, 'rb') as handle:
-            return pickle.load(handle)
+            return inverted_index
+
+    def get_text_from_doc_id(self, doc_id: int, filename: str) -> str:
+        """
+        Returns the text associated with an article id (doc_id).
+        :param doc_id: The document id to find the corresponding text of
+        :param filename: The filename of the file where the article is stored
+        :return: The contents of the <bdy> tag
+        """
+        with open(self._article_file_path + str(filename), 'r') as f:
+            data_stream = f.read()
+
+        data = BeautifulSoup(data_stream, 'lxml')
+        article_tag = data.find('id', text=doc_id)
+        text = article_tag.parent.parent.find('bdy').text
+        return text
