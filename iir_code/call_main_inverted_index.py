69a05b0f509969d2e78e9f2fcfddf0219a321ecf
Tobias Eidelpes <tobias@eidelpes.info>
2021-12-02 17:42:19 +0100

    Fix wrong imports due to changed folder structure

--- iir_code/call_main_inverted_index.py
+++ iir_code/call_main_inverted_index.py
@@ -1,8 +1,8 @@
 # create Inverted Index given wikipedia documents
 
 from iir_code.createindex import main
-from iir_code.inverted_index import InvertedIndex
-from iir_code.service.file_manager import FileManager
+from iir_code.data.inverted_index import InvertedIndex
+from iir_code.services.file_manager import FileManager
 
 file_manager = FileManager()
 inverted_index = InvertedIndex()
