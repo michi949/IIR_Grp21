8f85b259e72c7a8e86ee2878e303df9f5ac1af48
Arianna Pera <54574943+ariannap13@users.noreply.github.com>
2021-12-03 15:45:25 +0100

    compatibility with tfidf/bm25 and 100 docs filter

--- iir_code/evaluation_mode.py
+++ iir_code/evaluation_mode.py
@@ -3,35 +3,42 @@ This file contains your code to generate the evaluation files that are input to 
 """
 import subprocess
 
-from iir_code.exploration_mode import tf_idf, bm25
-from iir_code.services.file_manager import FileManager
+from exploration_new import tf_idf, bm25, read_index_from_file
+from FileManager import FileManager
 
 file_manager = FileManager()
 
 
 def main_exploration():
     topics = file_manager.read_topics_file()
+    
+    # Read the index
+    index = read_index_from_file()
 
     # TF-IDF Exploration
-    process_topics_query(topics, "TF-IDF")
+    process_topics_query(topics, "TF-IDF", index)
 
     # BM25 Exploration
-    process_topics_query(topics, "BM25")
+    process_topics_query(topics, "BM25", index)
 
 
-def perform_search(input_str: str, function_type: str):
+def perform_search(input_str: str, function_type: str, index):
     """
     Performs the search in the exploration file
     :param input_str: The search string with the title and the description of the topic
     :param function_type: The function type, could be BM25 or TF-IDF
-    :return: A dictionary of the result
+    :param index: the inverted index
+    :return: A dictionary of the result {doc_id:score} for each doc_id and for input_string
     """
+    dictionary = {}
+    
     if function_type == "TF-IDF":
-        return tf_idf(input_str)
+        dictionary = tf_idf(input_str, index)
+    
     elif function_type == "BM25":
-        return bm25(input_str)
-    else:
-        return {}
+        dictionary = bm25(input_str, index)
+    
+    return dictionary
 
 
 def process_trec_eval(qrel_lines, function_type: str):
@@ -48,28 +55,29 @@ def process_trec_eval(qrel_lines, function_type: str):
     subprocess.run(["trec_eval", "-m", "map", "-m", "ndcg_cut.10", "-m", "P.10", "-m", "recall.10", base_path, goal_path])
 
 
-def process_topics_query(topics: dict, function_type: str):
+def process_topics_query(topics: dict, function_type: str, index):
     """
-    # TODO: Get the top 100 ranked and scored results from tf_idf or bm25 / wait till this part is implemented
-    # TODO: Adjust to the corresponding result parameters
+    # TODO: Check trec eval implementation
     :param function_type: The function type, could be BM25 or TF-IDF
     :param topics: The topics from the xml file
     """
     qrels_lines = []
     for topic, sub_dictionary in topics.items():
-        results = perform_search(sub_dictionary["title"] + " " + sub_dictionary["description"], function_type)
-        for result_key, result_value in results.items():
-            line = topic + " Q0 " + result_key["document_id"] + " " + result_value["rank"] + " " + result_value[
-                "score"] + " " + function_type
-
-            print(line)
+        results = perform_search(sub_dictionary["title"] + " " + sub_dictionary["description"], function_type, index)
+        # sort index by score values
+        results = dict(sorted(results.items(), key=lambda item: item[1], reverse=True))
+        for result_key, result_value in list(results.items())[:100]:
+            rank = list(results.keys()).index(result_key) # get rank as index
+            line = topic + " Q0 " + str(result_key )+ " " + str(rank) + " " + str(result_value) + " " + function_type
+            # print(line)
             qrels_lines.append(line)
 
     process_trec_eval(qrels_lines, function_type)
 
 
-# main_exploration()
+main_exploration()
 process_trec_eval(["2010003 Q0 19243417 1",
                    "2010003 Q0 3256433 1",
                    "2010003 Q0 275014 1",
                    "2010003 Q0 298021 0"], "TD-IFD")
+
