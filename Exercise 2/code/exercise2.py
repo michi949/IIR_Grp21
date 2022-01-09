b883a0c093820c102af816628a65faffe8362a6c
Tobias Eidelpes <tobias@eidelpes.info>
2022-01-09 18:13:55 +0100

    Add functions for warmup and call centrally

--- Exercise 2/code/exercise2.py
+++ Exercise 2/code/exercise2.py
@@ -1,3 +1,8 @@
 """
 Add the code for the 2nd exercise to this file. You can add additional ".py" files to your code repository (e.g. helper functions, etc.).
 """
+from warmup import *
+
+if __name__ == "__main__":
+    model = get_model(model_file)
+    warmup(model)
--- Exercise 2/code/warmup.py
+++ Exercise 2/code/warmup.py
@@ -6,17 +6,24 @@ from sklearn.metrics.pairwise import cosine_similarity
 model_file = os.path.join('..', 'dataset', 'wiki-news-300d-1M-subword.vec')
 model = gensim.models.KeyedVectors.load_word2vec_format(model_file, binary=False, encoding='utf8')
 
-# Define pairs of words and compute cosine similarity
-pairs=[["cat","dog"], ["cat","Vienna"], ["Vienna","Austria"],
-       ["Austria","dog"]]
+def get_model(path):
+    return gensim.models.KeyedVectors.load_word2vec_format(path, binary=False, encoding='utf8')
 
-for pair in pairs:
-  first = model[pair[0]]
-  second = model[pair[1]]
-  cossim = cosine_similarity([first], [second])[0]
-  print(pair, cossim)
-  
-# Find top 3 most similar words
-print("Top 3 Vienna: ", model.most_similar("Vienna", topn=3))
-print("Top 3 Austria: ", model.most_similar("Austria", topn=3))
-print("Top 3 cat: ", model.most_similar("cat", topn=3))
+
+def warmup(model):
+    """Get most similar words."""
+    # Define pairs of words and compute cosine similarity
+    pairs=[["cat","dog"], ["cat","Vienna"], ["Vienna","Austria"],
+           ["Austria","dog"]]
+
+    for pair in pairs:
+      first = model[pair[0]]
+      second = model[pair[1]]
+      cossim = cosine_similarity([first], [second])[0]
+      print(pair, cossim)
+      
+    # Find top 3 most similar words
+    print("Top 3 Vienna: ", model.most_similar("Vienna", topn=3))
+    print("Top 3 Austria: ", model.most_similar("Austria", topn=3))
+    print("Top 3 cat: ", model.most_similar("cat", topn=3))
+
